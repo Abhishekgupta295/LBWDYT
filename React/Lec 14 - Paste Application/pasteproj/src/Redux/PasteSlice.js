a35968266270b35f0c3837c9f9646236f51e31f7
Abhishekgupta295 <141903838+Abhishekgupta295@users.noreply.github.com>
2024-11-25 18:24:20 +0530

    Add files via upload
    in office lappy , changes made on 25 dec.

---
+++ React/Lec 14 - Paste Application/pasteproj/src/Redux/PasteSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  pastes : localStorage.getItem("pastes")
+  ? JSON.parse(localStorage.getItem("pastes"))
+  : []
+}
+
+export const PasteSlice = createSlice({
+  name: 'paste',
+  initialState,
+  reducers: {
+    addToPastes : (state, action) => {
+      
+    },
+    updateToPastes : (state, action) => {
+      
+    },
+    resetAllPastes : (state, action) => {
+     
+    },
+    removeFromPastes : (state, action) => {
+        
+      },
+  },
+})
+
+
+export const { addToPastes, updateToPastes, resetAllPastes , removeFromPastes  } = PasteSlice.actions
+
+export default PasteSlice.reducer
